eda1b91f22e5e9eb80048db3e275b6d856f9ae58
Ben Wolfs <ben.wolfs96@gmail.com>
2025-01-21 11:41:23 +0100

    Add project files.

---
+++ BrouwerService/Repositories/BierlandContext.cs
@@ -0,0 +1,9 @@
+﻿using BrouwerService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BrouwerService.Repositories;
+
+public class BierlandContext(DbContextOptions<BierlandContext> options) : DbContext(options)
+{
+    public required DbSet<Brouwer> Brouwers { get; init; }
+}
---
+++ BrouwerService/Repositories/BrouwerRepository.cs
@@ -0,0 +1,32 @@
+﻿using BrouwerService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BrouwerService.Repositories;
+
+public class BrouwerRepository(BierlandContext context) : IBrouwerRepository
+{
+
+
+    public IQueryable<Brouwer> FindAll() => context.Brouwers.AsNoTracking();
+
+    public IQueryable<Brouwer> FindByBeginNaam(string begin) => context.Brouwers.AsNoTracking().Where(brouwer => brouwer.Naam.StartsWith(begin));
+
+    public Brouwer? FindById(int id) => context.Brouwers.Find(id);
+
+    public void Insert(Brouwer brouwer)
+    {
+        context.Brouwers.Add(brouwer);
+        context.SaveChanges();
+    }
+
+    public void Update(Brouwer brouwer)
+    {
+        context.Brouwers.Update(brouwer);
+        context.SaveChanges();
+    }
+    public void Delete(Brouwer brouwer)
+    {
+        context.Brouwers.Remove(brouwer);
+        context.SaveChanges();
+    }
+}
