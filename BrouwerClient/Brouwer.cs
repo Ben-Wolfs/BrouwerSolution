a5ea15afd8444a88908900c830d3ce30bfd7d3f9
Ben Wolfs <ben.wolfs96@gmail.com>
2025-01-22 14:35:39 +0100

    Hoofdstuk 13

---
+++ BrouwerClient/Brouwer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrouwerClient;
+
+internal class Brouwer
+{
+    public int Id { get; set; }
+    public required string Naam { get; set; }
+    public int Postcode { get; set; }
+    public required string Gemeente { get; set; }
+}
---
+++ BrouwerClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using BrouwerClient;
+using System.Net;
+
+Console.Write("id: ");
+var id = int.Parse(Console.ReadLine()!);
+using var client = new HttpClient();
+var uri = $"http://localhost:5200/brouwers/{id}";
+var response = await client.GetAsync(uri);
+
+//if (response.StatusCode == HttpStatusCode.Created)
+//{
+//    Console.WriteLine($$"""Brouwer is toegevoegd. Zijn URI is {{response.Headers.Location }}.""");
+//} else
+//{
+//    Console.WriteLine("Technisch probleem, contacteer de helpdesk.");
+//}
+
+switch (response.StatusCode)
+{
+    case HttpStatusCode.OK:
+        var brouwer = await response.Content.ReadAsAsync<Brouwer>();
+        brouwer.Gemeente = brouwer.Gemeente.ToUpper();
+        response= await client.PutAsJsonAsync(uri, brouwer);
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            Console.WriteLine("Brouwer gewijzigd");
+        }
+        else
+        {
+            Console.WriteLine("Technisch probleem, contacteer de helpdesk.");
+        }
+        break;
+    case HttpStatusCode.NotFound:
+        Console.WriteLine("Brouwer niet gevonden.");
+        break;
+    default:
+        Console.WriteLine("Technisch probleem, contacteer de helpdesk.");
+        break;
+}
+Console.ReadKey();
